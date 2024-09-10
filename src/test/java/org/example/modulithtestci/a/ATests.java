aeea3beb740e1de31de9b05d76240765444d6a93
Lukas Dohmen <l.dohmen@yahoo.de>
2024-09-10 21:41:13 +0200

    Setup project

---
+++ src/test/java/org/example/modulithtestci/a/ATests.java
@@ -0,0 +1,14 @@
+package org.example.modulithtestci.a;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class ATests {
+
+    @Test
+    void should() {
+
+        assertThat(true).isTrue();
+    }
+}
---
+++ src/test/java/org/example/modulithtestci/b/BTests.java
@@ -0,0 +1,13 @@
+package org.example.modulithtestci.b;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import org.junit.jupiter.api.Test;
+
+public class BTests {
+
+    @Test
+    void should() {
+        assertThat(true).isTrue();
+    }
+}
