f9e0c58fda01bad3025135da599ea2063e3a52db
Lukas Dohmen <l.dohmen@yahoo.de>
2024-09-10 21:24:26 +0200

    init

---
+++ src/main/java/org/example/modulithtestci/ModulithTestCiApplication.java
@@ -0,0 +1,13 @@
+package org.example.modulithtestci;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModulithTestCiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModulithTestCiApplication.class, args);
+    }
+
+}
